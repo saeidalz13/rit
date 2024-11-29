822da88743bcfc8040a3c99c9f154a5a3be33a17
Saeid Alizadeh <saeidalz96@gmail.com>
2024-11-28 19:46:02 -0700

    added basic structure of commit

--- src/cli/exec.rs
+++ src/cli/exec.rs
@@ -1,6 +1,6 @@
 use crate::{
     cli::commands,
-    ops::{add::add_rit, init::init_rit, status::status_rit},
+    ops::{add::add_rit, commit::commit_rit, init::init_rit, status::status_rit},
 };
 use std::path::PathBuf;
 
@@ -8,8 +8,8 @@ pub fn exec_cli() {
     let matches = commands::get_commands().get_matches();
 
     match matches.subcommand() {
-        Some(("add", sm)) => {
-            let paths = sm
+        Some(("add", sub_matches)) => {
+            let paths = sub_matches
                 .get_many::<PathBuf>("PATH")
                 .into_iter()
                 .flatten()
@@ -27,6 +27,16 @@ pub fn exec_cli() {
         },
 
         Some(("status", _)) => status_rit(),
+
+        Some(("commit", sub_matches)) => {
+            let commit_msg = sub_matches
+                .get_one::<String>("message")
+                .unwrap()
+                .trim()
+                .to_owned();
+
+            commit_rit(&commit_msg);
+        }
 
         _ => unreachable!(),
     }
